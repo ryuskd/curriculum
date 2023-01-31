73feca9ed0bf36440d3ec10bfff109721fccb7eb
ryuskd <123168454+ryuskd@users.noreply.github.com>
2023-01-31 17:17:30 +0900

    3章課題提出(中間テスト)
    FizzBuzz問題の課題。

---
+++ 3-1/index.php
@@ -0,0 +1,19 @@
+<?php
+    $i = 0;
+    $j = 0;
+    $k = 0;
+    define(BRtag,'<br>');
+    for ($i=1;$i<=100;$i++) {
+        $j = $i%3;
+        $k = $i%5;
+        if ($j === 0 && $k === 0) {
+            echo 'FizzBuzz!!'.BRtag;
+        } elseif ($j === 0) {
+            echo 'Fizz!'.BRtag;
+        } elseif ($k === 0) {
+            echo 'Buzz!'.BRtag;
+        } else {
+            echo $i.BRtag;
+        }
+    }
+?>
